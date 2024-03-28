4367d20cafa49b8ac871675df40c9060e7db7e22
hoanghieufro <hoanghieufro@gmail.com>
2024-03-28 16:17:05 +0700

    Add more keymaps

--- lua/hoanghieu/remap.lua
+++ lua/hoanghieu/remap.lua
@@ -1,6 +1,7 @@
 
 vim.g.mapleader = " "
--- vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
+-- vim.vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
+--
 
 vim.keymap.set("n", "<leader>ee",function() vim.cmd[[:Neotree toggle]] end)
 
@@ -79,3 +80,69 @@ vim.keymap.set('n', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 -- Terminal
 
 vim.keymap.set("n",'<C-t>',vim.cmd.ToggleTerm)
+
+-- Do things without affecting the registers
+vim.keymap.set("n", "x", '"_x')
+vim.keymap.set("n", "<Leader>p", '"0p')
+vim.keymap.set("n", "<Leader>P", '"0P')
+vim.keymap.set("v", "<Leader>p", '"0p')
+vim.keymap.set("n", "<Leader>c", '"_c')
+vim.keymap.set("n", "<Leader>C", '"_C')
+vim.keymap.set("v", "<Leader>c", '"_c')
+vim.keymap.set("v", "<Leader>C", '"_C')
+vim.keymap.set("n", "<Leader>d", '"_d')
+vim.keymap.set("n", "<Leader>D", '"_D')
+vim.keymap.set("v", "<Leader>d", '"_d')
+vim.keymap.set("v", "<Leader>D", '"_D')
+
+-- Increment/decrement
+vim.keymap.set("n", "+", "<C-a>")
+vim.keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+vim.keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
+-- Disable continuations
+vim.keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+vim.keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- Jumplist
+vim.keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+vim.keymap.set("n", "te", ":tabedit")
+vim.keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- Split window
+vim.keymap.set("n", "ss", ":split<Return>", opts)
+vim.keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Move window
+vim.keymap.set("n", "sh", "<C-w>h")
+vim.keymap.set("n", "sk", "<C-w>k")
+vim.keymap.set("n", "sj", "<C-w>j")
+vim.keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+vim.keymap.set("n", "<C-w><left>", "<C-w><")
+vim.keymap.set("n", "<C-w><right>", "<C-w>>")
+vim.keymap.set("n", "<C-w><up>", "<C-w>+")
+vim.keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+vim.keymap.set("n", "<C-j>", function()
+	vim.diagnostic.goto_next()
+end, opts)
+
+vim.keymap.set("n", "<leader>r", function()
+	require("craftzdog.hsl").replaceHexWithHSL()
+end)
+
+vim.keymap.set("n", "<leader>i", function()
+	require("craftzdog.lsp").toggleInlayHints()
+end)
